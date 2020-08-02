3c90b109cd43e788024e293bc19c8fe86343d79c
Sean McCollum <anincrediblyshortname@gmail.com>
2020-08-01 22:37:25 -0700

    fix: security, actions, and tests (#10)
    * fix: remove example package-locks, remove NPM_TOKEN for install, update a bunch of dependencies
    
    * fix: add back commitlint
    
    * fix: git version string matching check. better "test runner"

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 root="$(cd `dirname "${BASH_SOURCE[0]}"`; cd ..; pwd)"
+declare -a example_names
+declare -a example_codes
 
 function set_git {
   local from="$1"
@@ -12,10 +15,39 @@ function set_git {
   done
 }
 function test_example {
-  local example_path="$root/examples/$1"
+  local example_name="$1"
   shift
+  local example_path="$root/examples/$example_name"
   local args="$@ $example_path"
+  echo "testing example \"$1\" (diff-package-lock $args)"
   "$root/index.js" $args
+  exit_code=$?
+  example_names+=($example_name)
+  example_codes+=($exit_code)
+}
+
+function print_results {
+  local name
+  local code
+  local i=0
+  local exit_code=0
+  echo
+  while [[ $i -lt ${#example_names[@]} ]]; do
+    name=${example_names[$i]:-none}
+    code=${example_codes[$i]:-none}
+    i=$(( i + 1 ))
+    echo "[$name] exit code: $code"
+    if [[ $code -gt 0 ]]; then
+      exit_code=$code
+    fi
+  done
+  if [[ $exit_code -gt 0 ]]; then
+    echo "FAILURE"
+  else
+    echo "SUCCESS"
+  fi
+  echo
+  exit $exit_code
 }
 
 function onexit {
@@ -27,3 +59,5 @@ set_git git .git
 test_example basic react-15 react-16
 test_example lerna react-15 react-16
 test_example lerna lodash-4.0 lodash-4.1
+
+print_results
