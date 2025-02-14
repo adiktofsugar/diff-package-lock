d78883d9c48665237643f019aac0d8b523c27131
Sean McCollum <anincrediblyshortname@gmail.com>
2025-02-14 14:33:08 -0800

    feat(remove-dep-analysis): remove the dependency graph (#44)
    It's both slow and inaccurate
    
    BREAKING CHANGE: it no longer shows the dependency graph, which was a feature originally

--- index.js
+++ index.js
@@ -4,30 +4,25 @@ const path = require("path");
 const Tree = require("./lib/Tree");
 
 const usage = `
-usage: diff-package-lock [-h|--help][-x|--exclude name][--printed][--exit-code][--depth|-d depth] [<from>][<to>][<path>]
+usage: diff-package-lock [-h|--help][--exit-code] [<from>][<to>][<path>]
 
 Options
   --help, -h        Show help
-  --printed         Show printed dependencies a second time (default: true)
-  --exclude, -x     Exclude packages from highest found level. Repeat for more.
   --exit-code       Exit with exit code similar to diff (1 for changes, 0 for none)
-  --depth, -d       Specify number of times to recurse into dependency trees
 
 Arguments
   from Commitish to start from (default "HEAD")
   to   Commitish to end at (default "disk")
   path Path to diff (default to cwd)
       NOTE: this doesn't work like git diff and filter, it's specifiying the
-        root directory we read the package.json and package-lock.json files from
+        root directory we read the package-lock.json file from
 `;
 
 // eslint-disable-next-line import/order
 const argv = require("minimist")(process.argv.slice(2), {
-  boolean: ["help", "printed", "exit-code"],
+  boolean: ["help", "exit-code"],
   alias: {
     h: "help",
-    x: "exclude",
-    d: "depth",
   },
 });
 
@@ -60,13 +55,11 @@ if (args.length === 2) {
 
 async function go() {
   const [fromTree, toTree] = [tree1, tree2].map((t) => new Tree(t, { cwd }));
-  const packageChange = await fromTree.getPackageChange(toTree);
-  packageChange.printDependencyChanges({
-    maxDepth: argv.depth,
-    exclude: argv.exclude,
-    showPrinted: argv.printed,
-  });
-  if (packageChange.checkHasPrintableDependencyChanges() && argv["exit-code"]) {
+  const changes = await fromTree.getChanges(toTree);
+  for (const change of changes) {
+    console.log(` - ${change.toString()}`);
+  }
+  if (argv["exit-code"] && changes.length) {
     process.exit(1);
   }
 }
