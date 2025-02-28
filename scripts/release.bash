82e51d17550e67f8de11a05e022c528c584b04ee
Sean McCollum <anincrediblyshortname@gmail.com>
2025-02-27 23:42:04 -0800

    release: put all release logic back into the one script since github actions are weirdly limited

--- scripts/release.bash
+++ scripts/release.bash
@@ -6,15 +6,13 @@ root_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 dry=
 usage="
-release.bash [-h][-n]
+release.bash [-h][-n] [<ref>]
 -h help
 -n dry run
+<ref> a git identifier, defaults to last (semver) tag
 
-Reads changes from stdin and creates release based on max bump
-Input lines should be in this format:
-  <commit> <bump> <message>
-Example:
-  d78883d minor remove the dependency graph (#44)
+Release a new version based on changes since <ref>
+Exits with 2 if no changes detected
 "
 
 while getopts "hn" opt; do
@@ -33,35 +31,58 @@ while getopts "hn" opt; do
   esac
 done
 
+
+shift $((OPTIND - 1))
+ref="${1:-}"
+if [[ -z "$ref" ]]; then
+  last_semver_version="$(git tag --list "v*" | xargs npx semver | tail -n1)"
+  if [[ -z "$last_semver_version" ]]; then
+    echo "Could not determine last semver version" >&2
+    exit 1
+  fi
+  # NOTE: semver converts the tags to an actual version, so this outputs the version number
+  ref="v$last_semver_version"
+fi
+
+
 major_changes=()
 minor_changes=()
 patch_changes=()
 
 max_bump=0
+
 while read line; do
   commit_hash="${line%% *}"
   line="${line#* }"
-  bump="${line%% *}"
-  line="${line#* }"
-  message="${line}"
-  entry="${message} ($commit_hash)"
-  if [[ "$bump" == "major" ]]; then
-    max_bump=3
-    major_changes+=("$entry")
-  elif [[ "$bump" == "minor" && "$max_bump" != "major" ]]; then
-    max_bump=$(( max_bump > 2 ? max_bump : 2 ))
-    minor_changes+=("$entry")
-  elif [[ "$bump" == "patch" && "$max_bump" != "major" && "$max_bump" != "minor" ]]; then
-    max_bump=$(( max_bump > 1 ? max_bump : 1 ))
-    patch_changes+=("$entry")
+  commit_subject="${line}"
+  if [[ "$commit_subject" =~ ^([a-z]+?)(\(.+?\))?(!)?:\s*(.+) ]]; then
+    level="${BASH_REMATCH[1]}"
+    scope="${BASH_REMATCH[2]}" # note: still has parentheses
+    excl="${BASH_REMATCH[3]}"
+    message="${BASH_REMATCH[4]}"
+    message="${message# *}" # remove leading space
+
+    entry="${message} ($commit_hash)"
+    if [[ $excl ]]; then
+      max_bump=3
+      major_changes+=("$entry")
+    elif [[ "$level" == "feat" ]]; then
+      max_bump=$(( max_bump > 2 ? max_bump : 2 ))
+      minor_changes+=("$entry")
+    elif [[ "$level" == "fix" ]]; then
+      max_bump=$(( max_bump > 1 ? max_bump : 1 ))
+      patch_changes+=("$entry")
+    fi
   fi
-done
+done <<<"$(git log --pretty=format:"%h %s" HEAD ^"$ref")"
 
 if [[ $max_bump = 0 ]]; then
   echo "No changes detected" >&2
-  exit 1
+  exit 2
 fi
 
+bumps=(patch minor major)
+bump=${bumps[$max_bump - 1]}
 changelog="$(cat "$root_dir/CHANGELOG.md")"
 current_version="$(npm view . version)"
 next_version="$(npx semver "$current_version" -i $bump)"
