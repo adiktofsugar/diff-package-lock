013842d39ab21a8ad24cb45c4043bab54234a3c1
Sean McCollum <anincrediblyshortname@gmail.com>
2025-02-27 23:02:05 -0800

    release: separate print changes and actual release so I don't have to checkout twice

--- scripts/release.bash
+++ scripts/release.bash
@@ -9,7 +9,12 @@ usage="
 release.bash [-h][-n]
 -h help
 -n dry run
-Release based on changes since last tag
+
+Reads changes from stdin and creates release based on max bump
+Input lines should be in this format:
+  <commit> <bump> <message>
+Example:
+  d78883d minor remove the dependency graph (#44)
 "
 
 while getopts "hn" opt; do
@@ -28,79 +33,70 @@ while getopts "hn" opt; do
   esac
 done
 
-# NOTE: semver converts the tags to an actual version, so this outputs the version number
-latest_version="$(git tag --list "v*" | xargs npx semver | tail -n1)"
-if [[ -z "$latest_version" ]]; then
-  echo "No tags found" >&2
+major_changes=()
+minor_changes=()
+patch_changes=()
+
+max_bump=0
+while read line; do
+  commit_hash="${line%% *}"
+  line="${line#* }"
+  bump="${line%% *}"
+  line="${line#* }"
+  message="${line}"
+  entry="${message} ($commit_hash)"
+  if [[ "$bump" == "major" ]]; then
+    max_bump=3
+    major_changes+=("$entry")
+  elif [[ "$bump" == "minor" && "$max_bump" != "major" ]]; then
+    max_bump=$(( max_bump > 2 ? max_bump : 2 ))
+    minor_changes+=("$entry")
+  elif [[ "$bump" == "patch" && "$max_bump" != "major" && "$max_bump" != "minor" ]]; then
+    max_bump=$(( max_bump > 1 ? max_bump : 1 ))
+    patch_changes+=("$entry")
+  fi
+done
+
+if [[ $max_bump = 0 ]]; then
+  echo "No changes detected" >&2
   exit 1
 fi
 
 changelog="$(cat "$root_dir/CHANGELOG.md")"
-
-is_breaking=
-feats=()
-fixes=()
-
-while read commit_subject; do
-  if [[ "$commit_subject" =~ ^([a-z]+?)(\(.+?\))?(!)?:\s*(.+) ]]; then
-    level="${BASH_REMATCH[1]}"
-    scope="${BASH_REMATCH[2]}" # note: still has parentheses
-    excl="${BASH_REMATCH[3]}"
-    message="${BASH_REMATCH[4]}"
-    if [[ $excl ]]; then
-      is_breaking=true
-    fi
-    echo "-- detected level: $level, scope: $scope, breaking: $excl, message: $message"
-    if [[ "$level" == "feat" ]]; then
-      feats+=("$message")
-    elif [[ "$level" == "fix" ]]; then
-      fixes+=("$message")
-    fi
-  fi
-done <<<"$(git log --pretty=format:"%s" HEAD ^"v$latest_version")"
-
-bump=
-if [[ "$is_breaking" ]]; then
-  bump="major"
-elif [[ "${#feats[@]}" -gt 0 ]]; then
-  bump="minor"
-elif [[ "${#fixes[@]}" -gt 0 ]]; then
-  bump="patch"
-fi
-if ! [[ $bump ]]; then
-  echo "No changes detected since last tag"
-  exit 0
-fi
-
-next="$(npx semver "$latest_version" -i $bump)"
-
-
+current_version="$(npm view . version)"
+next_version="$(npx semver "$current_version" -i $bump)"
 newline="
 "
-changelog_patch="## $next ($(date +%Y-%m-%d))$newline$newline"
-if [[ ${#feats[@]} -gt 0 ]]; then
-  changelog_patch+="### Features$newline$newline"
-  for msg in "${feats[@]}"; do
-    changelog_patch+="* $msg$newline"
-  done
-  changelog_patch+="$newline"
+changelog_patch="## $next_version ($(date +%Y-%m-%d))$newline$newline"
+function add_changes() {
+  local level="$1"
+  local messages=("${@:2}")
+  if [[ ${#messages[@]} -gt 0 ]]; then
+    changelog_patch+="### $level$newline$newline"
+    for msg in "${messages[@]}"; do
+      changelog_patch+="* $msg$newline"
+    done
+    changelog_patch+="$newline"
+  fi
+}
+if [[ ${#major_changes[@]} -gt 0 ]]; then
+  add_changes "Breaking" "${major_changes[@]}"
 fi
-if [[ ${#fixes[@]} -gt 0 ]]; then
-  changelog_patch+="### Fixes$newline$newline"
-  for msg in "${fixes[@]}"; do
-    changelog_patch+="* $msg$newline"
-  done
-  changelog_patch+="$newline"
+if [[ ${#minor_changes[@]} -gt 0 ]]; then
+  add_changes "Features" "${minor_changes[@]}"
+fi
+if [[ ${#patch_changes[@]} -gt 0 ]]; then
+  add_changes "Fixes" "${patch_changes[@]}"
 fi
 
 if [[ -n "$dry" ]]; then
   echo "New Changelog: $newline===$newline$changelog_patch==="
   echo "Bump level: $bump"
-  echo "Next version: $next"
+  echo "Next version: $next_version"
 else
   echo "$changelog_patch$newline$newline$changelog" > "$root_dir/CHANGELOG.md"
   # npm version command makes git tag and commit
-  npm version --no--git-tag-version "$next" 
+  npm version --no-git-tag-version "$next_version" 
   git add "$root_dir"
-  git commit -m "chore(release): $next"
+  git commit -m "chore(release): $next_version"
 fi
